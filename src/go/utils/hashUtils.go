dd227f1abd0027d8ec871aa60608c3bc17c66ed6
oladeleseyi <toothy61@gmail.com>
2023-07-05 00:16:57 +0100

    hash utils

---
+++ src/go/utils/hashUtils.go
@@ -0,0 +1,21 @@
+package utils
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+)
+
+func hashString(input string) string {
+	inputBytes := []byte(input)
+
+	// Create a new SHA256 hash object
+	hash := sha256.New()
+	// Write the input bytes to the hash object
+	hash.Write(inputBytes)
+
+	// Calculate the SHA256 hash
+	hashBytes := hash.Sum(nil)
+
+	// Convert the hash bytes to a hexadecimal string
+	return hex.EncodeToString(hashBytes)
+}
